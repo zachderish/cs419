f885af4b5b0d7d94c74727e818a6757e5212693c
Zach Derish <zderish@gmail.com>
2024-02-21 10:16:37 -0500

    Update scrypt xor logic

--- scrypt.py
+++ scrypt.py
@@ -25,13 +25,25 @@ file2 = open(text2, mode="w")
 
 key = sdbm(password)
 char = file1.read(1)
-while char:
-    print(key)
 
-    key_string = str(key)
-    key_encoded = key_string.encode()
-    cipher_char = bytes((a ^ b) for a, b in zip(char, key_encoded))
-    cipher_char = cipher_char.decode('ascii', errors='replace')
-    file2.write(cipher_char)
-    char = file1.read(1)
+while char:
+    #print(key)
     key = keystream(key)
+    #key_string = str(key)
+    #key_encoded = key_string.encode()
+    char_int = int.from_bytes(char, "little")
+    print(char_int, key)
+    #cipher_char = bytes((a ^ b) for a, b in zip(char, key_encoded))
+    cipher_char = key ^ char_int
+    print(cipher_char)
+    if 0 <= cipher_char <= 127:
+        cipher_char = chr(cipher_char)
+        file2.write(cipher_char)
+    else:
+        file2.write('\uFFFD')
+    #cipher_char = cipher_char.decode('ascii', errors='replace')
+    #cipher_char = str(cipher_char)
+    char = file1.read(1)
+
+print(bytes((a ^ b) for a, b in zip(b'T', b'150')))
+print(150 ^ 1010100)
